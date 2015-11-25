1a405b903cbaf90dafdd87c0b20adddc10bbc21b
Eddy <scott.edward@aurora.aero>
2015-11-25 16:19:33 -0500

    Updated cmake to allow for git submodule.  Changed launch file to look at usb_cam rectified image

--- catkin.cmake
+++ catkin.cmake
@@ -40,33 +40,35 @@ catkin_package(
 # CGAL requires that -frounding-math be set.
 add_definitions(-frounding-math)
 
-# Download the external Swatbotics repository.
+# Download the external apriltags-cpp repository.
 include(ExternalProject)
-ExternalProject_Add(apriltags_swatbotics_EXTERNAL
-    GIT_REPOSITORY https://github.com/personalrobotics/apriltags-cpp
+ExternalProject_Add(apriltags-cpp
+    DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}
+    SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/apriltags-cpp
+    BINARY_DIR ${PROJECT_BINARY_DIR}/external/apriltags-cpp-build
+    STAMP_DIR ${PROJECT_BINARY_DIR}/external/apriltags-cpp-stamp
+    TMP_DIR ${PROJECT_BINARY_DIR}/external/apriltags-cpp-tmp
     INSTALL_COMMAND ""
-    BUILD_COMMAND "make" # I don't know why this works...
+    BUILD_COMMAND "make"
     CMAKE_ARGS -DCMAKE_CXX_FLAGS=-frounding-math -DBUILD_SHARED_LIBS:BOOL=ON
 )
 
-# Recover project paths for additional settings.
-ExternalProject_Get_Property(apriltags_swatbotics_EXTERNAL
-  SOURCE_DIR BINARY_DIR INSTALL_DIR)
-set(apriltags_swatbotics_INCLUDE_DIRS
-  "${SOURCE_DIR}"
-)
-set(apriltags_swatbotics_LIBRARIES
+ExternalProject_Get_Property(apriltags-cpp SOURCE_DIR BINARY_DIR INSTALL_DIR)
+
+message(STATUS "HEY YOU STUPID FUCK")
+message(STATUS ${SOURCE_DIR})
+# Tell CMake that the external project generated a library so we
+# can add dependencies here instead of later.
+set(apriltags_cpp_LIBRARIES
   "${BINARY_DIR}/libapriltags.so"
 )
 
-# Tell CMake that the external project generated a library so we
-# can add dependencies here instead of later.
-add_library(apriltags_swatbotics UNKNOWN IMPORTED)
-set_property(TARGET apriltags_swatbotics
+add_library(apriltags-cpp-libs UNKNOWN IMPORTED)
+set_property(TARGET apriltags-cpp-libs
   PROPERTY IMPORTED_LOCATION
-  ${apriltags_swatbotics_LIBRARIES}
+  ${apriltags_cpp_LIBRARIES}
 )
-add_dependencies(apriltags_swatbotics apriltags_swatbotics_EXTERNAL)
+add_dependencies(apriltags-cpp-libs apriltags-cpp)
 
 include_directories(
     include/
@@ -74,7 +76,7 @@ include_directories(
     ${Eigen_INCLUDE_DIRS}
     ${OpenCV_INCLUDE_DIRS}
     ${Yaml_INCLUDE_DIRS}
-    ${apriltags_swatbotics_INCLUDE_DIRS}
+    ${SOURCE_DIR}
 )
 
 add_executable(apriltags src/apriltags.cpp)
@@ -82,13 +84,13 @@ target_link_libraries(apriltags ${catkin_LIBRARIES})
 target_link_libraries(apriltags ${Eigen_LIBRARIES})
 target_link_libraries(apriltags ${OpenCV_LIBRARIES})
 target_link_libraries(apriltags ${Yaml_LIBRARIES})
-target_link_libraries(apriltags apriltags_swatbotics)
+target_link_libraries(apriltags apriltags-cpp-libs)
 add_dependencies(apriltags ${PROJECT_NAME}_gencpp)
 
 install(TARGETS apriltags
   RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION} 
 )
 
-install(FILES ${apriltags_swatbotics_LIBRARIES}
+install(FILES ${apriltags_cpp_LIBRARIES}
   DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
 )
